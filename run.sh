5ac6b0f225916185387ef75865bb3686093b9d20
ghzhang <zhangguanhua1996@gmail.com>
2021-09-09 19:38:08 +0800

    update run order

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
-# Configure the Vim
-git clone https://github.com/ghzhang233/Vim-Zarth.git ~/Vim-Zarth
-./~/Vim-Zarth/run.sh
-
 # Configure the aliases
 if [ ! -f .bash_aliases ]; then
     cp ./.bash_aliases ~/.
 else
     cat ./.bash_aliases >> ~/.bash_aliases
 fi
+
+# Configure the Vim
+git clone https://github.com/ghzhang233/Vim-Zarth.git ~/Vim-Zarth
+cd ~/Vim-Zarth
+bash run.sh
