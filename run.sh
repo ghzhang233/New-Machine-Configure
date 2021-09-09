94684dc78d973180b753d0ad2b32027d3aea659a
ghzhang <zhangguanhua1996@gmail.com>
2021-09-09 20:05:12 +0800

    add anaconda

--- run.sh
+++ run.sh
@@ -5,7 +5,22 @@ else
     cat ./.bash_aliases >> ~/.bash_aliases
 fi
 
+if ( ! grep ./.bash_aliases ~/.bashrc ) 
+then
+    cat 'if [ -f ~/.bash_aliases ]; then\n    . ~/.bash_aliases\nfi'
+fi
+
 # Configure the Vim
 git clone https://github.com/ghzhang233/Vim-Zarth.git ~/Vim-Zarth
 cd ~/Vim-Zarth
 bash run.sh
+
+# Configure the Anaconda
+cd ~
+wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+bash Anaconda3-2021.05-Linux-x86_64.sh
+cd ~/anaconda3/bin
+./conda init
+srrc
+conda update conda
+conda update anaconda
